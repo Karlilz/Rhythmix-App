3667dfc3a1f1cb88be442e99bb582ca6ebc48db1
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-11-04 14:32:44 -0400

    playlists model added

--- backend/playlists/models.py
+++ backend/playlists/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 class Playlist(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
+'''
 class Song(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
     playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+'''
--- backend/playlists/serializers.py
+++ backend/playlists/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
-from .models import Playlist, Song
+from .models import Playlist
 
 class PlaylistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Playlist
         fields = '__all__'
 
+'''
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = '__all__'
+'''
