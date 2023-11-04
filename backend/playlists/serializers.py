6e54b225dea28c41cd45c3ad3bed8ff00a77e123
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-11-04 16:14:08 -0400

    playlists model working

--- backend/playlists/serializers.py
+++ backend/playlists/serializers.py
@@ -1,6 +1,4 @@
 from rest_framework import serializers
-
-'''
 from .models import Playlist
 
 class PlaylistSerializer(serializers.ModelSerializer):
@@ -8,4 +6,3 @@ class PlaylistSerializer(serializers.ModelSerializer):
         model = Playlist
         fields = '__all__'
 
-'''
